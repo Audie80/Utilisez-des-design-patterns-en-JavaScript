63409c3e946c5ccaefc59773dbe8da0c46ae1968
Aude Velly Menut <audie.menut@gmail.com>
2022-02-13 09:06:49 +0000

    add OldMovie model class

--- js/App.js
+++ js/App.js
@@ -5,9 +5,11 @@ class App {
     }
 
     async main() {
-        const movies = await this.moviesApi.getMovies()
+        const moviesData = await this.moviesApi.getMovies()
 
-        movies.forEach(movie => {
+        moviesData
+            .map(movie => new OldMovie(movie))
+            .forEach(movie => {
             const Template = new MovieCard(movie)
             this.$moviesWrapper.appendChild(Template.createMovieCard())        
         })    
