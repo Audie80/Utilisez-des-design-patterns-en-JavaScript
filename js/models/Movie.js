5b5ff8c52ab3a32dead99437d38157f169005845
Aude Velly Menut <audie.menut@gmail.com>
2022-02-13 09:25:50 +0000

    Change data to new-movie so change model

--- js/models/Movie.js
+++ js/models/Movie.js
@@ -1,3 +1,35 @@
 class Movie {
+    constructor(data){
+        this._title = data.title;
+        this._synopsis = data.synopsis;
+        this._picture = data.picture;
+        this._duration = data.duration;
+        this._released_in = data.released_in;
+    }
 
+    get title() {
+        return this._title.fr ? this._title.fr : this._title.en
+    }
+
+    get synopsis() {
+        return this._synopsis;
+    }
+
+    get picture() {
+        return `/assets/${this._picture}`;
+    }
+
+    get thumbnail() {
+        return `/assets/thumbnails/${this._picture}`;
+    }
+
+    get duration() {
+        const nombreHeures = Math.floor(this._duration / 60);
+        const nombreMinutes = this._duration - (nombreHeures * 60);
+        return `${nombreHeures}h${nombreMinutes}`;
+    }
+
+    get released_in() {
+        return this._released_in;
+    }
 }
